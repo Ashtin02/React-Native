52d9089e53c1e2423bd3dd3deb7d05fca57b35f6
mx-cifu <cifuentesmax1@gmail.com>
2025-03-02 14:05:00 -0800

    Created most of the skeleton with some changes

--- Studying-App/app/_layout.tsx
+++ Studying-App/app/_layout.tsx
@@ -1,39 +1,98 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
-import * as SplashScreen from 'expo-splash-screen';
-import { StatusBar } from 'expo-status-bar';
-import { useEffect } from 'react';
-import 'react-native-reanimated';
+import {DrawerContentComponentProps,} from "@react-navigation/drawer";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
+import { Drawer } from "expo-router/drawer";
+import { StyleSheet, View, Text, TouchableOpacity } from "react-native";
+import { useRouter } from "expo-router";
 
-import { useColorScheme } from '@/hooks/useColorScheme';
-
-// Prevent the splash screen from auto-hiding before asset loading is complete.
-SplashScreen.preventAutoHideAsync();
-
-export default function RootLayout() {
-  const colorScheme = useColorScheme();
-  const [loaded] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
-  });
-
-  useEffect(() => {
-    if (loaded) {
-      SplashScreen.hideAsync();
-    }
-  }, [loaded]);
-
-  if (!loaded) {
-    return null;
-  }
-
+export default function Layout() {
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
-      </Stack>
-      <StatusBar style="auto" />
-    </ThemeProvider>
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <Drawer drawerContent={(props) => <CustomDrawerContent {...props} />}
+       screenOptions = {{
+        headerStyle: {
+          backgroundColor: '#D9D9D9',
+        },
+      }}
+      >
+        <Drawer.Screen
+          name="index"
+          options={{
+            drawerLabel: "Home",
+            title: "Index",
+          }}
+        />
+        <Drawer.Screen
+          name="dashboard"
+          options={{
+            drawerLabel: "Dashboard",
+            title: "Dashboard",
+          }}
+        />
+        <Drawer.Screen
+          name="settings"
+          options={{
+            drawerLabel: "Settings",
+            title: "Settings",
+          }}
+        />
+      </Drawer>
+    </GestureHandlerRootView>
   );
 }
+
+function CustomDrawerContent(props: DrawerContentComponentProps) {
+  const router = useRouter();
+
+  return (
+    <View style={{ flex: 1, justifyContent: "space-between", backgroundColor: '#F3EDF7' }}>
+      {/*  Drawer Items */}
+      <View>
+        <TouchableOpacity onPress={() => router.push("/")} style={[styles.drawerItem, styles.drawerItemTop]}>
+          <Text style={styles.drawerText}>Home</Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => router.push("/dashboard")} style={styles.drawerItem}>
+          <Text style={styles.drawerText}>Dashboard</Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => router.push("/settings")} style={[styles.drawerItem, styles.drawerItemBottom]}>
+          <Text style={styles.drawerText}>Settings</Text>
+        </TouchableOpacity>
+      </View>
+
+      {/*  Bottom Drawer Item */}
+      <TouchableOpacity onPress={() => router.push("/logout")} style={styles.logoutItem}>
+        <Text style={styles.logoutText}>Logout</Text>
+      </TouchableOpacity>
+    </View>
+  );
+}
+
+const styles =  StyleSheet.create({
+  drawerItemTop: {
+    borderTopWidth: 2,
+    borderBottomWidth: 0,
+  },
+  drawerItem: {
+    padding: 15,
+    alignItems: "center",
+    borderStyle: 'solid',
+    borderTopWidth: 2,
+    borderBottomWidth: 2,
+    backgroundColor: "#E5D9EC"
+  },
+  drawerItemBottom: {
+    borderTopWidth: 0,
+    borderBottomWidth: 2,
+  },
+  drawerText: {
+    fontSize: 18,
+  },
+  logoutItem: {
+    padding: 16,
+    backgroundColor: "#E5D9EC",
+    alignItems: "center",
+    borderTopWidth: 2,
+  },
+  logoutText: {
+    fontSize: 18,
+  },
+});
---
+++ Studying-App/app/index.tsx
@@ -0,0 +1,27 @@
+import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
+import { useRouter } from 'expo-router';
+
+export default function Home() {
+  const router = useRouter();
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Welcome to My  Project!
+      </Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#fff',
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 20,
+  },
+});
