d726685fbcdfd50ab609d42257b1fae6e0b0df46
Ashtin <ashtinrivada@yahoo.com>
2025-03-20 15:31:08 -0700

    Finished home page, new design, added link to help page, added button to login page

--- Studying-App/app/index.tsx
+++ Studying-App/app/index.tsx
@@ -7,7 +7,28 @@ export default function Home() {
 
   return (
     <View style={styles.container}>
-      <Flashcard question='What is the best way to study?' answer='With flashcards!'/>
+
+      <View style={styles.titleContainer}>
+        <Text style={styles.title}>Welcome to FlipFocus!</Text>
+        <Text style={styles.subHeading}>Learn interactively with flashcards!</Text>
+      </View>
+
+      <View style={styles.flashcardContainer}>
+        <Flashcard question='What is the best way to study?' answer='With flashcards!' />
+      </View>
+
+      <View style={styles.buttonContainer}>
+        <TouchableOpacity style={styles.button} onPress={()=> router.push("/login")}>
+          <Text style={styles.buttonText}>Login</Text>
+        </TouchableOpacity>
+      </View>
+
+      <View style={styles.footer}>
+        <TouchableOpacity onPress={()=> router.push("/help")}>
+          <Text style={styles.footerText}>need help?</Text>
+        </TouchableOpacity>
+      </View>
+
     </View>
   );
 }
@@ -18,10 +39,51 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#fff',
+    padding: 20,
+  },
+  titleContainer: {
+    alignItems: "center",
+    marginBottom:20,
   },
   title: {
-    fontSize: 24,
+    fontSize: 30,
     fontWeight: 'bold',
-    marginBottom: 20,
+    color: '#4a90e2',
   },
+  subHeading: {
+    fontSize: 16,
+    color: '#888',
+    marginTop: 10,
+  }, 
+  footer: {
+    position: 'absolute',
+    bottom: 10,
+  },
+  footerText: {
+    fontSize: 12,
+    color: '#4a90e2',
+    textDecorationLine: 'underline',
+  },
+  flashcardContainer: {
+    flex: 2,
+    justifyContent: "center",
+    elevation:5,
+  },
+  buttonText: {
+    color: '#fff',
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+  buttonContainer: {
+    marginTop: 20,
+    marginBottom:20,
+  },
+  button: {
+    backgroundColor: '#4a90e2',
+    paddingVertical: 12,
+    paddingHorizontal: 30,
+    borderRadius: 25,
+    elevation:5,
+  },
+  
 });
